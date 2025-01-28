7a668da326cf8c9ad3663a385a521baea3ccbf5c
Philip Smith <philip@randomsmiths.com>
2025-01-28 17:51:58 -0500

    fix urls

--- superfablab/visit_tracking/urls.py
+++ superfablab/visit_tracking/urls.py
@@ -5,7 +5,7 @@ app_name = 'station'
 
 urlpatterns = [
     path('scan/', views.scan, name='scan'),
-    path('set_forgot', views.set_forgot, name='set_forgot')
+    path('set_forgot', views.set_forgot, name='set_forgot'),
     path("new-user/<int:niner_id>/", views.new_user_form, name="new_user_form"),
     path('close/', views.close_space, name="close_space")
 
