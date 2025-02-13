23e3b5511e5b28004646e205979fc45751a6f92e
Philip Smith <philip@randomsmiths.com>
2025-02-13 16:17:40 -0500

    make it so that only people who can train can see it

--- superfablab/tools_and_trainings/views.py
+++ superfablab/tools_and_trainings/views.py
@@ -12,8 +12,11 @@ from django.contrib.auth import get_user_model
 def staff_required(view_func):
     return user_passes_test(lambda u: u.space_level >= get_user_model().SpaceLevel.KEYHOLDER)(view_func)
 
+def certify_ability(view_func):
+    return user_passes_test(lambda u: Training.objects.filter(user=u, training_level__gte=Training.TrainingLevels.TRAINER).exists() )(view_func)
 
-@staff_required
+
+@certify_ability
 def create_training(request):
     if request.POST:
         print(f"POST {request.POST}")
@@ -24,11 +27,26 @@ def create_training(request):
 
         return redirect("home")
     
+    available_trainings = {}
+    
+    for training in TrainingCategory.objects.all():
+        try:
+            to = Training.objects.get(category=training, user=request.user, training_level__gte=Training.TrainingLevels.TRAINER)
+            available_trainings[training] = []
+            for training_level in Training.TrainingLevels.choices:
+                if training_level[0] < to.training_level:
+                    available_trainings[training].append(training_level)
+                    continue
+                    
+                if to.training_level == Training.TrainingLevels.CERTIFIER:
+                    available_trainings[training].append(training_level)
+        except Exception as e:
+            print(Training.objects.get(category=training, user=request.user))
+        
     
     context = {
         "current_visits": list(Visit.objects.get_signed_in_users()),
-        "available_trainings": list(TrainingCategory.objects.all()),
-        "training_levels": Training.TrainingLevels.choices
+        "available_trainings": available_trainings,
     }
     
     return render(request, "training_form.html", context)
