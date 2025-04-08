9823d2b9bbf7b768089ac2d1bd1f55ad5f7f8af3
Philip Smith <philip@randomsmiths.com>
2025-04-08 00:50:10 -0400

    make the profile toolbar hidden unless you are local

--- superfablab/superfablab/settings.py
+++ superfablab/superfablab/settings.py
@@ -198,8 +198,3 @@ SERVER_EMAIL = "super-fab-lab@c4glenn.com"  # ditto (default from-email for Djan
 
 
 INTERNAL_IPS = ['127.0.0.1', '172.18.0.1']
-
-
-DEBUG_TOOLBAR_CONFIG = {
-    'SHOW_TOOLBAR_CALLBACK': lambda request: True,
-}
