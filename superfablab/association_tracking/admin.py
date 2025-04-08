1510b1b2fac9f58cea62f46d08d24e2255a82323
Philip Smith <philip@randomsmiths.com>
2025-04-08 15:48:27 -0400

    add admin and make email nullable

--- superfablab/association_tracking/admin.py
+++ superfablab/association_tracking/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import AssociationEntities, Association
 
-# Register your models here.
+@admin.register(AssociationEntities)
+class AssociationEntitiesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'short_name', 'contact')
+    search_fields = ('short_name', 'description')
+    list_filter = ('contact',)
+
+@admin.register(Association)
+class AssociationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'user', 'entity')
+    search_fields = ('email',)
+    list_filter = ('entity', 'user')
--- superfablab/association_tracking/models.py
+++ superfablab/association_tracking/models.py
@@ -22,16 +22,21 @@ class AssociationEntities(models.Model):
     contact = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="contact", blank=True, null=True)
 
     objects = AssocationEntityManager()
+    
+    def __str__(self):
+        return self.short_name
 class AssociationManager(models.Manager):
     def new_user_add(self, email):
         user = SpaceUser.objects.get(email=email)
         for association in self.filter(email=email, user__isnull=True).all():
             association.user = user
             association.save()
+    def create_associaton(self, entity: AssociationEntities, niner_id=None, email=None):
+        pass
 
 class Association(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user", blank=True, null=True)
-    email = models.EmailField()
+    email = models.EmailField(null=True, blank=True)
     entity = models.ForeignKey(AssociationEntities, on_delete=models.CASCADE, related_name="entity")
 
     objects = AssociationManager()
