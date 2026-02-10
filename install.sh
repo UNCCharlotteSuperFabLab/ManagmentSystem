df36a3fd14e905f3ff1a8f5ee8441e68bf753594
Anthony DiBernardo <adibern2@charlotte.edu>
2026-02-09 20:17:15 -0500

    Disable and remove management.service before moving

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+systemctl disable management.service
+rm /etc/systemd/system/management.service
 mv ./management.service /etc/systemd/system
 systemctl start management.service
 systemctl enable management.service
